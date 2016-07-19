0d669ce1875853d9f49b36081e7e0c0b31430e98
astronaut-wannabe <contact@astronaut-wannabe.com>
2016-07-19 19:39:55 -0400

    try new package-list setup for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,11 +9,14 @@
 
 (require 'package)
 (setq package-enable-at-startup nil)
-(setq package-archives '(
-                         ;; ("org"              . "http://orgmode.org/elpa/") 
-                         ("gnu"             . "http://elpa.gnu.org/packages/")
-                         ("melpa"         . "http://melpa.milkbox.net/packages/")))
-                         ;;("marmalade" . "http://marmalade-repo.org/packages/")))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+;; (setq package-archives '(
+;;                          ;; ("org"              . "http://orgmode.org/elpa/") 
+;;                          ("gnu"             . "http://elpa.gnu.org/packages/")
+;;                          ("melpa"         . "http://melpa.milkbox.net/packages/")))
+;;                          ;;("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (package-initialize)
 
