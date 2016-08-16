307ef64685427ca23f4e73554f10a9cdeed5fc34
astronaut-wannabe <contact@astronaut-wannabe.com>
2016-08-16 13:02:54 -0700

    [EMACS] move auto-generated stuff to ignored file
    Followed this blog post: https://web.archive.org/web/20160318212126/http://emacsblog.org/2008/12/06/quick-tip-detaching-the-custom-file/

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,23 +9,8 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
+;; move all the auto-generated stuff to a separate file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
-;; auto-generated stuff
-;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (railscasts)))
- '(custom-safe-themes
-   (quote
-    ("3b0a350918ee819dca209cec62d867678d7dac74f6195f5e3799aa206358a983" default)))
- '(package-selected-packages (quote (org company lorem-ipsum))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:foreground "red" :height 3.0)))))
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
